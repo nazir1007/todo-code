b64f2ae49d57f7616f02162fb524db8217e0941c
ansari-saif <saifhussainansari6@gmail.com>
2022-07-02 22:09:08 +0530

    react setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const Component = () => <h1>hello saif</h1>
+const App = () => {
+    return (
+        <div><Component /></div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
